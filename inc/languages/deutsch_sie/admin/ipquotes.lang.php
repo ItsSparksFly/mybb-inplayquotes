d81cb062a0e9a7b9a03cd32fac1e2a04a0eb70a9
Julia Roloff <72280520+ItsSparksFly@users.noreply.github.com>
2021-06-28 17:58:24 +0200

    added admin language deutsch_sie

--- inc/languages/deutsch_sie/admin/ipquotes.lang.php
+++ inc/languages/deutsch_sie/admin/ipquotes.lang.php
@@ -1,3 +1,7 @@
 <?php
-
+$l['inplayquotes'] = "Inplayzitate";
+$l['inplayquotes_desc'] = "Erlaubt es Mitgliedern, Zitate aus Beiträgen einzufügen, die dann auf dem Index des Forums erscheinen.";
+$l['inplayquotes_settings'] = "Einstellungen für die Inplayzitate";
+$l['inplayquotes_boards'] = "Inplay-Kategorien";
+$l['inplayquotes_boards_desc'] = "Wähle die Kategorien/Foren aus, in denen der Inplayzitat-Button angezeigt werden soll.";
 ?>
